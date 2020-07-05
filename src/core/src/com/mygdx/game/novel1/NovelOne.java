ebd55d4649935f7b56bb49baf4c49ab4650e2566
Ron <ronald.tran12@gmail.com>
2020-07-05 15:05:09 -0400

    hello world

--- src/core/src/com/mygdx/game/novel1/NovelOne.java
+++ src/core/src/com/mygdx/game/novel1/NovelOne.java
@@ -4,24 +4,30 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Color;
+
 
 public class NovelOne extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		font.setColor(Color.RED);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		font.draw(batch, "Hello World", 200, 200);
 		batch.end();
 	}
 	
@@ -29,5 +35,20 @@ public class NovelOne extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height){
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
 	}
 }
