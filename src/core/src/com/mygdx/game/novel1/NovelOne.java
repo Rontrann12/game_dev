172ee88dfd491848c81f773fb767871bd111a0b9
Ron <ronald.tran12@gmail.com>
2020-07-08 20:36:26 -0400

    initial development

--- src/core/src/com/mygdx/game/novel1/NovelOne.java
+++ src/core/src/com/mygdx/game/novel1/NovelOne.java
@@ -5,21 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Color;
+import com.mygdx.game.novel1.constants.Constants;
+import com.mygdx.game.novel1.flow.GameFlowHandler;
 
 
 public class NovelOne extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	BitmapFont font;
+	GameFlowHandler menu;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture(Constants.IMAGE_PATH+"menu/title_page.png");
+		sprite = new Sprite(img);
 		font = new BitmapFont();
 		font.setColor(Color.RED);
+		menu = new GameFlowHandler();
 	}
 
 	@Override
@@ -27,8 +34,11 @@ public class NovelOne extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		sprite.draw(batch);
 		font.draw(batch, "Hello World", 200, 200);
 		batch.end();
+
+
 	}
 	
 	@Override
---
+++ src/core/src/com/mygdx/game/novel1/flow/GameFlowHandler.java
@@ -0,0 +1,5 @@
+package com.mygdx.game.novel1.flow;
+
+public class GameFlowHandler {
+    private static Boolean startup = false;
+}
