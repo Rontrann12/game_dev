68d3fa3e9e6c1f4aa2fe938bd427ed98f282bd30
Ron <ronald.tran12@gmail.com>
2020-08-01 21:59:15 -0400

    adding choice buttons to in game ui

--- src/core/src/com/mygdx/game/novel1/ui/buttons/ChoiceButton.java
+++ src/core/src/com/mygdx/game/novel1/ui/buttons/ChoiceButton.java
@@ -5,33 +5,26 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.mygdx.game.novel1.NovelOne;
-import com.mygdx.game.novel1.screens.InGame;
 
+public class ChoiceButton extends BaseButton {
 
-/**
- * Action of this button will change the screen to InGame
- * TODO - Should probaly be renamed more appropriately (ToGameButton)
- */
-public class StartButton extends BaseButton {
-
-    public StartButton(TextureRegion idleTexture, TextureRegion hoverTexture, final NovelOne game, final String name) {
+    public ChoiceButton(TextureRegion idleTexture, TextureRegion hoverTexture, final NovelOne game, final String name) {
         super(idleTexture, hoverTexture, game, name);
 
         addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-
-                Gdx.app.log(name, "Start button clicked");
+                Gdx.app.log(name, "Choice button clicked");
 
                 action(game);
-
                 return true;
             }
         });
+
     }
 
     @Override
     public void action(NovelOne game) {
-        game.setScreen(new InGame(game));
+
     }
 }
