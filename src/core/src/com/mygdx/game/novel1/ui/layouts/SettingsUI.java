951e96d4480e0967b2fb33e3fa12e0b68ad3eca7
unknown <ronald.tran12@gmail.com>
2020-12-21 01:28:53 -0500

    adding base files for screens that need to be implemented

---
+++ src/core/src/com/mygdx/game/novel1/ui/layouts/SettingsUI.java
@@ -0,0 +1,16 @@
+package com.mygdx.game.novel1.ui.layouts;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.mygdx.game.novel1.NovelOne;
+
+public class SettingsUI extends BaseLayout {
+
+    public SettingsUI(Stage stage, NovelOne game) {
+        super(stage, game);
+    }
+
+    @Override
+    public void generateUI(){
+
+    }
+}
